19361de2aba6079aa228a0a5df0519c0b2b04397
Ed Shnekendorf <eshneken@gmail.com>
2017-08-14 16:25:01 -0400

    remove docker registry details and parameterize app name

--- chef/cookbooks/bmcs_servers/recipes/docker_weblogic.rb
+++ chef/cookbooks/bmcs_servers/recipes/docker_weblogic.rb
@@ -5,18 +5,16 @@ end
 
 # set docker registry from attributes and assign container name
 docker_registry = node['docker_registry_ip_and_port']
-container_name = docker_registry + "/weblogic-1221"
+container_name = docker_registry + "/" + node['application_tag']
 
 # Start Docker
 docker_service 'default' do
 	action [:create, :start]
 	tls false
-	# insecure_registry '129.213.60.3:5000'
     insecure_registry docker_registry
 end
 
 # Pull tagged image
-# docker_image '129.213.60.3:5000/weblogic-1221' do
 docker_image container_name do
 	tag 'latest'
 	action :pull
@@ -24,7 +22,6 @@ end
 
 # Run container
 docker_container 'weblogic' do
-	# repo '129.213.60.3:5000/weblogic-1221'
     repo container_name
 	tag 'latest'
 	port '7001:7001'
