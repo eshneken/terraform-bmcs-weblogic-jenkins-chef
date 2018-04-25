aae473e711830c81ec2579be096a6f3111f159cb
Ed Shnekendorf <eshneken@gmail.com>
2018-04-25 14:33:28 -0400

    update to later docker versions

--- chef/cookbooks/bmcs_servers/metadata.rb
+++ chef/cookbooks/bmcs_servers/metadata.rb
@@ -7,8 +7,8 @@ long_description 'Installs/Configures Oracle BMCS Servers'
 version '0.2.7'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-depends 'docker', '~> 2.0'
-depends 'firewall', '~> 2.0'
+depends 'docker', '~> 4.2.0'
+depends 'firewall', '~> 2.6.3'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
